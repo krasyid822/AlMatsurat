5842627a46bf309832660107415fac210848ac77
krasyid822 <krasyid822@gmail.com>
2024-12-17 20:32:16 +0700

    .

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,9 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('woff2-cache').then(function(cache) {
         return cache.addAll([
-          'rsc\\woff2\\material-icon.woff2'
+          'rsc\\woff2\\material-icon.woff2',
+          'https://github.com/krasyid822/AlMatsurat/releases/download/1/Al.Matsurat.Pagi.opus',
+          'https://github.com/krasyid822/AlMatsurat/releases/download/1/Rabithah_ai.opus'
         ]);
       })
     );
